5f3434e82fe7f21c9a881b356ff3a2dd853429ad
maftunabshark-blip <maftunabshark@gmail.com>
2025-09-15 16:50:57 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'smeta-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png',
+  './icons/apple-touch-icon.png'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
